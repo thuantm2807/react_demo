81f15424ff573592188f26d902bcb14b63f731ec
thuan.tran <thuan.tran@southtelecom.vn>
2023-08-03 18:55:28 +0700

    Updates

---
+++ precache-manifest.2bcbfc84e5512dc2a423f0bce4ab74c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "42f740e2fddca82db6de",
+    "url": "/react_demo/static/css/main.e91b3396.chunk.css"
+  },
+  {
+    "revision": "42f740e2fddca82db6de",
+    "url": "/react_demo/static/js/main.8a37755b.chunk.js"
+  },
+  {
+    "revision": "89f9c4554954967d73b7",
+    "url": "/react_demo/static/js/runtime~main.b88ba7fe.js"
+  },
+  {
+    "revision": "1cfeb9e626d8dad0937a",
+    "url": "/react_demo/static/css/2.969bd299.chunk.css"
+  },
+  {
+    "revision": "1cfeb9e626d8dad0937a",
+    "url": "/react_demo/static/js/2.39830d21.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/react_demo/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "014a0a4e52e3774d7540d5d17b3c85c6",
+    "url": "/react_demo/static/media/NeNe_WeNo_Reliev.014a0a4e.woff"
+  },
+  {
+    "revision": "f8e48ddd3c54f5bdafeaff1e588b62d2",
+    "url": "/react_demo/index.html"
+  }
+];
